aeb5a2fa14ebd5079f47a3d11f3c9ac652a73707
HYEONSIK <ks06891@naver.com>
2025-02-19 17:15:27 +0900

    feat: gradle.yml 파일 수정
    - 자동 배포를 위한 yml 파일 수정
    - health 코드 추가

---
+++ src/main/java/org/dfbf/soundlink/global/Controller.java
@@ -0,0 +1,19 @@
+package org.dfbf.soundlink.global;
+
+import lombok.RequiredArgsConstructor;
+import org.dfbf.soundlink.global.exception.ErrorCode;
+import org.dfbf.soundlink.global.exception.ResponseResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class Controller {
+
+    // health check
+    @GetMapping("/health")
+    public ResponseResult health() {
+        return new ResponseResult(ErrorCode.SUCCESS,"ok");
+    }
+}

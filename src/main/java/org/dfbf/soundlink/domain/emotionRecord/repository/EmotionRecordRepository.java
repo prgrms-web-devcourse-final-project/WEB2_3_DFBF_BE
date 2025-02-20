2833aa8b02a9cd96b5c4c49e22a8e5af3df7995a
JO HUIJI <hjjo0927@gmail.com>
2025-02-20 10:17:24 +0900

    feat: 감정 기록 전체 조회 기능 추가 #3

--- src/main/java/org/dfbf/soundlink/domain/emotionRecord/repository/EmotionRecordRepository.java
+++ src/main/java/org/dfbf/soundlink/domain/emotionRecord/repository/EmotionRecordRepository.java
@@ -21,6 +21,12 @@ public interface EmotionRecordRepository extends JpaRepository<EmotionRecord, Lo
             "FROM EmotionRecord er " +
             "WHERE er.user = :user" )
     List<EmotionRecordDto> findByUser(@Param("user") User user);
+
+    @Query( "SELECT er FROM EmotionRecord er " +
+            "JOIN FETCH er.user u " +
+            "LEFT JOIN FETCH er.spotifyMusic sm " +
+            "WHERE u.userId = :userId ")
+    List<EmotionRecord> findByUserId(@Param("userId") Long userId);
 }
 
 /**

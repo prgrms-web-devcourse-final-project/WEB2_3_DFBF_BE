1452f3c8e396b18ad9fe18a841338da96bd04b02
JO HUIJI <hjjo0927@gmail.com>
2025-02-21 10:12:05 +0900

    feat: 유저별 감정 기록 조회 기능 추가 #3
    해당 기능 추가 하면서 감정기록 조회 응답 관련 DTO 분리 및 기존 기능에 대한 메서드명 변경함

--- src/main/java/org/dfbf/soundlink/domain/emotionRecord/dto/response/EmotionRecordResponseWithOwnerDTO.java
+++ src/main/java/org/dfbf/soundlink/domain/emotionRecord/dto/response/EmotionRecordResponseWithOwnerDTO.java
@@ -4,37 +4,32 @@ import org.dfbf.soundlink.domain.emotionRecord.entity.EmotionRecord;
 
 import java.sql.Timestamp;
 import java.time.format.DateTimeFormatter;
-import java.util.List;
-import java.util.stream.Collectors;
 
-public record EmotionRecordResponseMainDTO(
+public record EmotionRecordResponseWithOwnerDTO(
         Long recordId,
         String nickName,
         String emotion,
         SpotifyMusicResponseDTO spotifyMusic,
         String comment,
-        String createAt
-        ) {
-
-    public static EmotionRecordResponseMainDTO fromEntity(EmotionRecord record) {
-        return new EmotionRecordResponseMainDTO(
+        String createAt,
+        boolean isOwner // 유저 본인 여부 추가
+) {
+    public static EmotionRecordResponseWithOwnerDTO fromEntity(EmotionRecord record, Long requestUserId) {
+        return new EmotionRecordResponseWithOwnerDTO(
                 record.getRecordId(),
                 record.getUser().getNickName(),
                 record.getEmotion().name(),
                 record.getSpotifyMusic() != null ? SpotifyMusicResponseDTO.fromEntity(record.getSpotifyMusic()) : null,
                 record.getComment(),
-                formatTimestamp(record.getCreatedAt())
+                formatTimestamp(record.getCreatedAt()),
+                record.getUser().getUserId().equals(requestUserId) // 요청한 유저와 저장된 유저 비교
         );
-    }
-
-    public static List<EmotionRecordResponseMainDTO> fromEntities(List<EmotionRecord> records) {
-        return records.stream().map(EmotionRecordResponseMainDTO::fromEntity).collect(Collectors.toList());
     }
 
     private static String formatTimestamp(Timestamp timestamp) {
         DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-
         if (timestamp == null) return null;
         return timestamp.toLocalDateTime().format(FORMATTER);
     }
 }
+

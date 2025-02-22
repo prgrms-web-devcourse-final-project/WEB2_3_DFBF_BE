8675a7af3d31f4904d7fdc05acdee3c024719d02
HYEONSIK <ks06891@naver.com>
2025-02-22 19:41:21 +0900

    fix: CORS 에러 해결
    - allowedOrigin에서 allowedOriginPatterns으로 변경

--- src/main/java/org/dfbf/soundlink/global/config/WebConfig.java
+++ src/main/java/org/dfbf/soundlink/global/config/WebConfig.java
@@ -6,8 +6,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    //    private static final String DEVELOP_FRONT_ADDRESS = "http://localhost:3000";
-//
+    /**
+     * 스프링부트 5.3 이후,
+     * allowCredentials가 true일 때, allowedOrigins에 특수 값인 "*" 추가할 수 없게 되었다.
+     * 대신 allowOriginPatterns를 사용해야 한다.
+     */
+
+     private static final String DEVELOP_FRONT_ADDRESS = "http://localhost:3000";
+
+     // Main에 적용
 //    @Override
 //    public void addCorsMappings(CorsRegistry registry) {
 //        registry.addMapping("/**")
@@ -18,13 +25,14 @@ public class WebConfig implements WebMvcConfigurer {
 //                .allowCredentials(true);
 //    }
 
+    // Develop에 적용
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 엔드포인트
-                .allowedOrigins("*")          // 모든 도메인 허용
+                .allowedOriginPatterns("*")       // 모든 도메인 허용
                 .allowedMethods("*")          // 모든 HTTP 메서드 허용
                 .allowedHeaders("*")          // 모든 헤더 허용
                 .exposedHeaders("*")          // 모든 응답 헤더 허용
-                .allowCredentials(false);     // 인증 정보는 제외
+                .allowCredentials(true);     // 인증 정보는 제외
     }
 }

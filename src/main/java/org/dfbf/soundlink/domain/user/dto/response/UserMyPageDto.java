6c191b94d5fbfff79d860b2a321e1f7c63d4502c
HYEONSIK <ks06891@naver.com>
2025-02-27 10:33:30 +0900

    fix: 유저 정보 조회 수정
    - SpotifyId값을 Long에서 String으로 변경해서 생긴 에러 수정

--- src/main/java/org/dfbf/soundlink/domain/user/dto/response/UserMyPageDto.java
+++ src/main/java/org/dfbf/soundlink/domain/user/dto/response/UserMyPageDto.java
@@ -1,18 +1,15 @@
 package org.dfbf.soundlink.domain.user.dto.response;
 
-import lombok.Getter;
-import lombok.Setter;
-import org.dfbf.soundlink.domain.emotionRecord.entity.EmotionRecord;
-
-import java.util.List;
+import lombok.*;
 
 @Getter
+@AllArgsConstructor
 public class UserMyPageDto {
     private final String loginId;
     private final String nickname;
     private final ProfileMusic profileMusic;
 
-    public UserMyPageDto(String loginId, String nickname, Long spotifyId, String title, String artist, String album) {
+    public UserMyPageDto(String loginId, String nickname, String spotifyId, String title, String artist, String album) {
         this.loginId = loginId;
         this.nickname = nickname;
         this.profileMusic = new ProfileMusic(spotifyId, title, artist, album);
@@ -26,4 +23,4 @@ public class UserMyPageDto {
 }
 
 // Inner 레코드
-record ProfileMusic(Long spotifyId, String title, String artist, String album) {}
+record ProfileMusic(String spotifyId, String title, String artist, String album) {}

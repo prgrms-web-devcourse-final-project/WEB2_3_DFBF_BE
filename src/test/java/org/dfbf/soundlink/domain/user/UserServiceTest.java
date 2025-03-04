99ee78731645cbc02861106bbb4f694c0daa00c5
Hyeromon <trina9018@gmail.com>
2025-03-04 17:45:46 +0900

    [feat] 회원가입 및 탈퇴 테스트 코드 작성 #49

--- src/test/java/org/dfbf/soundlink/domain/user/UserServiceTest.java
+++ src/test/java/org/dfbf/soundlink/domain/user/UserServiceTest.java
@@ -1,13 +1,27 @@
 package org.dfbf.soundlink.domain.user;
 
+import org.dfbf.soundlink.domain.emotionRecord.repository.EmotionRecordRepository;
 import org.dfbf.soundlink.domain.emotionRecord.repository.SpotifyMusicRepository;
+import org.dfbf.soundlink.domain.user.dto.request.UserSignUpDto;
+import org.dfbf.soundlink.domain.user.entity.User;
 import org.dfbf.soundlink.domain.user.repository.UserRepository;
 import org.dfbf.soundlink.domain.user.service.UserService;
+import org.dfbf.soundlink.global.exception.ErrorCode;
+import org.dfbf.soundlink.global.exception.ResponseResult;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.Optional;
+
+import static org.dfbf.soundlink.global.comm.enums.SocialType.KAKAO;
+import static org.mockito.Mockito.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
@@ -22,6 +36,9 @@ public class UserServiceTest {
 
     @Mock
     private BCryptPasswordEncoder passwordEncoder;
+
+    @Mock
+    private EmotionRecordRepository emotionRecordRepository;
 
     // 회원 정보 수정
 //    @Test
@@ -50,4 +67,46 @@ public class UserServiceTest {
 //        verify(existingUser).update(eq(userUpdateDto), any(BCryptPasswordEncoder.class), eq(spotifyMusic));
 //    }
 
+    @Test
+    @DisplayName("회원가입 테스트 성공 - 비밀번호 암호화 및 저장")
+    void signUp_Success(){
+        //given
+        UserSignUpDto userSignUpDto = new UserSignUpDto(
+                "testNickname", 12345L, KAKAO, "testLoginId", "password123", "test@example.com"
+        );
+        //when
+        //save 메소드 호출 시 모킹 처리
+        when(passwordEncoder.encode(userSignUpDto.password())).thenReturn("encodedPassword");
+
+        ResponseResult result = userService.signUp(userSignUpDto);
+        //then
+        assertEquals(200, result.getCode());
+
+        verify(userRepository, times(1)).save(any(User.class));
+
+        verify(passwordEncoder, times(1)).encode(userSignUpDto.password());
+    }
+
+    @Test
+    @DisplayName("회원삭제 성공")
+    void deleteUser_Success(){
+        //given
+        Long userId = 1L;
+        User user = mock(User.class);
+
+        // 유저를 찾기
+        when(userRepository.findByUserIdWithCache(userId)).thenReturn(Optional.of(user));
+        // 감정 기록 삭제, doNotion()-> 단지 메소드가 호출되었는지만 확인
+        doNothing().when(emotionRecordRepository).deleteByUser(user);
+        // 유저 삭제
+        doNothing().when(userRepository).deleteById(userId);
+
+        //when
+        ResponseResult result = userService.deleteUser(userId);
+
+        //then
+        assertEquals(200, result.getCode());
+        verify(emotionRecordRepository, times(1)).deleteByUser(user); //(1): 1번 호출되었는지 확인 = 감정기록이 삭제 됨.
+//        verify(userRepository, times(0)).deleteById(userId);//(0):호출되지 않았는지 확인 = 사용자가 삭제 되지 않음.(1):성공적으로 삭제됨.
+    }
 }

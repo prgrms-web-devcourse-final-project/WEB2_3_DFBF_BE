e7058d9a06f49268550d0394fb937a9d3f9588a8
moon-kun-woong <plm303333@gmail.com>
2025-03-04 15:11:39 +0900

    [test] created blocklist controller & service testCode.

---
+++ src/test/java/org/dfbf/soundlink/CustomLogger.java
@@ -0,0 +1,13 @@
+package org.dfbf.soundlink;
+
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.TestWatcher;
+
+public class CustomLogger implements TestWatcher {
+    public void testSuccessful(ExtensionContext context) {
+        System.out.println("통과 되었습니다! -> " + context.getDisplayName() + " checked!");
+    }
+    public void testFailed(ExtensionContext context, Throwable cause) {
+        System.out.println("불확실한 테스트 입니다 -> " + context.getDisplayName() + " failed...");
+    }
+}

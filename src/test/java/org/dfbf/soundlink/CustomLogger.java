fc8421e5a024528f2f2aad5181bda9f49203454d
moon-kun-woong <plm303333@gmail.com>
2025-03-04 16:52:36 +0900

    [test] fixed customLogger method annotation override.

--- src/test/java/org/dfbf/soundlink/CustomLogger.java
+++ src/test/java/org/dfbf/soundlink/CustomLogger.java
@@ -4,9 +4,11 @@ import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.TestWatcher;
 
 public class CustomLogger implements TestWatcher {
+    @Override
     public void testSuccessful(ExtensionContext context) {
         System.out.println("통과 되었습니다! -> " + context.getDisplayName() + " checked!");
     }
+    @Override
     public void testFailed(ExtensionContext context, Throwable cause) {
         System.out.println("불확실한 테스트 입니다 -> " + context.getDisplayName() + " failed...");
     }

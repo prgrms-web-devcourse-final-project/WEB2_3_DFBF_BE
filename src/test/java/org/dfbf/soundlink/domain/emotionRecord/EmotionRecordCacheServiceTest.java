f14c7c96523064d2813c62ca0361da91aeea0954
JO HUIJI <hjjo0927@gmail.com>
2025-03-10 17:47:45 +0900

    fix: SonarCube 이슈 수정 #64

--- src/test/java/org/dfbf/soundlink/domain/emotionRecord/EmotionRecordCacheServiceTest.java
+++ src/test/java/org/dfbf/soundlink/domain/emotionRecord/EmotionRecordCacheServiceTest.java
@@ -25,7 +25,7 @@ import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-public class EmotionRecordCacheServiceTest {
+class EmotionRecordCacheServiceTest {
     @Mock
     private RedisTemplate<String, Object> redisTemplate;
 

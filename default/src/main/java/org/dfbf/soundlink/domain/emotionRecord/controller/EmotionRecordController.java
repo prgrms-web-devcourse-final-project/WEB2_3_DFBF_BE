fbfb6c9addbca29136cdfa9ea3980080d4b58526
HYEONSIK <ks06891@naver.com>
2025-06-09 18:17:18 +0900

    feat: 감정일기 v2 #112
    - 중복요청(따닥) 방지 기능 추가

--- default/src/main/java/org/dfbf/soundlink/domain/emotionRecord/controller/EmotionRecordController.java
+++ default/src/main/java/org/dfbf/soundlink/domain/emotionRecord/controller/EmotionRecordController.java
@@ -33,6 +33,18 @@ public class EmotionRecordController {
         return emotionRecordService.saveEmotionRecordWithMusic(userId, request);
     }
 
+    @PostMapping("/v2")
+    @Operation(
+            summary = "감정 기록 작성/저장 API",
+            description = "작성한 감정 기록을 저장 & Idempotency Key를 사용하여 중복 요청을 방지."
+    )
+    public ResponseResult saveEmotionWithMusicAndIdempotency(
+            @RequestHeader(name = "Idempotency-Key") String idempotencyKey,
+            @AuthenticationPrincipal Long userId,
+            @Valid @RequestBody EmotionRecordRequestDTO request) {
+        return emotionRecordService.saveEmotionRecordWithMusicAndIdempotent(idempotencyKey, userId, request);
+    }
+
     @GetMapping
     @Operation(
             summary = "감정 기록 전체 조회 메인 API",
